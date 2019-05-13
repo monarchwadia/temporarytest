80a2f84ca374f919040c2194c3279bb31f66b084
DESKTOP-RIDGAOB\Robert <robwhyte12@gmail.com>
2019-05-13 15:34:16 -0400

    Cleaned up code, Flushed out player movement system, game is feature complete

--- AdventrueGame/AdventrueGame/main.cpp
+++ AdventrueGame/AdventrueGame/main.cpp
@@ -12,8 +12,11 @@ int main()
 	bool inForest = false;
 	char playerInput;
 
+	string forestText = "You walk through a dark and gloomy forest.";
+	string plainsText = "You walk through the sunny plains, it is a beautiful day.";
+
 	cout << "You are on the plains, to the North you see a forest, to the East, West, and South you see Plains.  What do you do?" << endl;
-	cout << "Use N, S, E, W to move";
+	cout << "Use N, S, E, W to move" << endl;
 
 	while (playing)
 	{
@@ -23,20 +26,23 @@ int main()
 		switch (playerInput) {
 		case 'N':
 			playerY++;
-			if (playerY > 0)
-			{
-				inForest == true;
-				cout << "You walk through a dark and gloomy forest." << endl;
-			}
-			else
-			{
-				inForest == false;
-				cout << "You walk through the sunny plains, it is a beautiful day." << endl;
-			}
 			break;
-
+		case 'S':
+			playerY--;
+			break;
+		case 'E':
+			break;
+		case 'W':
+			break;
 		}
-
+		if (playerY > 0)
+		{
+			cout << forestText << endl;
+		}
+		else
+		{
+			cout << plainsText << endl;
+		}
 	}
 
 
